30c9de17b72dfefd6276d49ac66f2a8acde79e05
Deivyt G <116587097+Deivyt2608@users.noreply.github.com>
2025-02-28 21:29:45 -0500

    BotonUpgrade

--- TarjetaPresentación/java.js
+++ TarjetaPresentación/java.js
@@ -2,3 +2,11 @@ window.addEventListener("load", function() {
     document.body.classList.add("loaded");
     document.querySelector('.basico').classList.add("loaded");
 });
+
+function funClick1(){
+    if (document.getElementById("parr1").innerText === "Septimo semestre universidad EAN") {
+        document.getElementById("parr1").innerText = "Me gustaria enfocarme en el cloud computing";
+    } else {
+        document.getElementById("parr1").innerText = "Septimo semestre universidad EAN";
+    }
+};
